74b936948679f6d6afc252c8ebdf2bc92ea1aa1f
Anh Kiệt <anhkiet>
2023-10-20 10:48:50 +0700

    upload source code

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Lâm Nhi';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20/10';
+const titleCard = 'Tặng bà nè';
+const contentCard = 'Chúc bà ngày 20/10 vui vẻ nha, ngày càng xinh đẹp hơn và cười nhiều hơn, đặc biệt là đừng suy nghĩ nhiều hay buồn nữa nha bà';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'nhi1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
